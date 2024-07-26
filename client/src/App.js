6d6cb7adcf2a4f9b6afc6014d1ada087a37d016c
Rajiv Ravindra pathak <70104100+rajivravindrapathak@users.noreply.github.com>
2024-07-26 19:52:09 +0530

    Add files via upload

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import Homepage from './pages/Homepage';
+import './assets/css/style.scss'
+
+function App() {
+  return (
+    <div className="App">
+      <Homepage />
+    </div>
+  );
+}
+
+export default App;
