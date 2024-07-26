4c36f3e72a95555529a661f8a1bd25191ab9cb79
Rajiv Ravindra pathak <70104100+rajivravindrapathak@users.noreply.github.com>
2024-07-26 20:20:48 +0530

    Add files via upload

--- server/index.js
+++ server/index.js
@@ -1,76 +1,3 @@
-// const express = require('express');
-// const http = require('http');
-// const socketIo = require('socket.io');
-// const Redis = require('ioredis');
-// const mongoose = require('mongoose');
-// const bodyParser = require('body-parser');
-// const { connection } = require('./config/db');
-
-// const app = express();
-// const server = http.createServer(app);
-// const io = socketIo(server);
-
-// const redis = new Redis({
-//     port: 12675,
-//     host: 'redis-12675.c212.ap-south-1-1.ec2.cloud.redislabs.com',
-//     username: '',
-//     password: 'dssYpBnYQrl01GbCGVhVq2e4dYvUrKJB'
-// });
-
-// const taskSchema = new mongoose.Schema({
-//     task: String
-// });
-
-// const Task = mongoose.model('assignment_<YOUR_FIRST_NAME>', taskSchema);
-
-// app.use(bodyParser.json());
-
-// app.get('/', () => {
-//     res.send('get data')
-// })
-
-// io.on('connection', (socket) => {
-//     console.log('New client connected');
-
-//     socket.on('add', async (task) => {
-//         let tasks = await redis.get('FULLSTACK_TASK_<YOUR_FIRST_NAME>');
-//         tasks = tasks ? JSON.parse(tasks) : [];
-//         tasks.push(task);
-
-//         if (tasks.length > 50) {
-//             await Task.insertMany(tasks.map(task => ({ task })));
-//             tasks = [];
-//         }
-
-//         await redis.set('FULLSTACK_TASK_<YOUR_FIRST_NAME>', JSON.stringify(tasks));
-//         socket.emit('tasks', tasks);
-//     });
-
-//     socket.on('disconnect', () => {
-//         console.log('Client disconnected');
-//     });
-// });
-
-// app.get('/fetchAllTasks', async (req, res) => {
-//     let tasks = await redis.get('FULLSTACK_TASK_<YOUR_FIRST_NAME>');
-//     tasks = tasks ? JSON.parse(tasks) : [];
-//     res.json(tasks);
-// });
-
-// const PORT = process.env.PORT || 6001;
-
-// app.listen(PORT, async ()=> {    
-//     try {
-//         await connection;
-//         console.log("connected to DB")
-//     } catch (err) {
-//         console.log("not connected to db")    
-//         console.log(err)
-//     }
-//     console.log(`listning on PORT ${PORT}`)
-// })
-
-
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
@@ -99,7 +26,7 @@ const taskSchema = new mongoose.Schema({
     task: String
 });
 
-const Task = mongoose.model('assignment_<YOUR_FIRST_NAME>', taskSchema);
+const Task = mongoose.model('assignment_rajiv', taskSchema);
 
 app.use(bodyParser.json());
 
@@ -111,7 +38,7 @@ io.on('connection', (socket) => {
     console.log('New client connected');
 
     socket.on('add', async (task) => {
-        let tasks = await redis.get('FULLSTACK_TASK_<YOUR_FIRST_NAME>');
+        let tasks = await redis.get('FULLSTACK_TASK_rajiv');
         tasks = tasks ? JSON.parse(tasks) : [];
         tasks.push(task);
 
@@ -120,7 +47,7 @@ io.on('connection', (socket) => {
             tasks = [];
         }
 
-        await redis.set('FULLSTACK_TASK_<YOUR_FIRST_NAME>', JSON.stringify(tasks));
+        await redis.set('FULLSTACK_TASK_rajiv', JSON.stringify(tasks));
         io.emit('tasks', tasks); // Emit to all clients
     });
 
@@ -130,7 +57,7 @@ io.on('connection', (socket) => {
 });
 
 app.get('/fetchAllTasks', async (req, res) => {
-    let tasks = await redis.get('FULLSTACK_TASK_<YOUR_FIRST_NAME>');
+    let tasks = await redis.get('FULLSTACK_TASK_rajiv');
     tasks = tasks ? JSON.parse(tasks) : [];
     res.json(tasks);
 });
